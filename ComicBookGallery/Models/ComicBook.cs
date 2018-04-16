a54d4fecd1afe62cdcb323bd9d8406ce809ae341
Danny Maitland <DannyMaitland180@outlook.com>
2018-04-16 20:30:10 +0100

    Added ComicBook and Artist Models.

---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+
+        public int id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public artist[] Artists { get; set; }
+        public bool Favourite { get; set; }
+
+    }
+}
